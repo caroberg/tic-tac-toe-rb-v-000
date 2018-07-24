5951a41c17128cb7382407eb623b5ca99fa03b55
Caroline Berg <carolineeberg@gmail.com>
2018-07-24 16:36:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,6 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    binding.pry
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
