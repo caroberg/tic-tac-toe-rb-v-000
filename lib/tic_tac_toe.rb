61d8fb00f32f081d1cbf3e6f31cb84beef56eafb
Caroline Berg <carolineeberg@gmail.com>
2018-07-24 16:27:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,9 +74,9 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      return true
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      return true
     else
       return false
     end
