2da78c4e7efe2f0c031fbb0e53d705e01f8266ee
Caroline Berg <carolineeberg@gmail.com>
2018-07-24 15:27:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,9 @@ def position_taken?(board, position)
   board[position] != " "
 end
 
-valid_move?(position) = position.between?(1, 9) && position == " "
+def valid_move?(position) 
+  position.between?(1, 9) && position == " "
+end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
